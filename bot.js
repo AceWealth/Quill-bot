1ac6f1900db4dad3b900b21499cf9692eddd278a
cmarmil <caileymillerit@gmail.com>
2019-05-27 18:48:29 -0400

    file cleanup

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
+//https://discord.js.org/#/
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const {
     RichEmbed
 } = require("discord.js");
-const config = require("./config.json");
+//https://github.com/sindresorhus/got << Node http request library
 const got = require('got');
-const errorMsg = require('./error-messages.js');
 
-//command exports
-//const randomPoem = require('./Rand-Poem.js');
+//local scripts
+const config = require("./config.json");
+const errorMsg = require('./error-messages.js');
 
 ///bot start
 client.on('ready', () => {
@@ -20,8 +21,8 @@ const prefix = "!";
 
 client.on("message", (message) => {
     if (!message.content.startsWith(prefix)) return;
-    // Exit and stop if it's not there
-     
+    // Exit and stop if prefix is not there
+
     //command to pull up a random poem from www.poemist.com API. 
     if (message.content.startsWith(prefix + "random poem")) {
         (async () => {
