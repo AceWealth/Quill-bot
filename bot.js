c36af38af0a530c96d57dc8b07c7fed123731835
cmarmil <caileymillerit@gmail.com>
2019-06-02 23:07:15 -0400

    added character gen. NPM link; change to official package when published.

--- bot.js
+++ bot.js
@@ -6,15 +6,19 @@ const {
 } = require("discord.js");
 //https://github.com/sindresorhus/got << Node http request library
 
+//CHANGE TO NPM PACKAGE WHEN PUBLISHED
+const generate = require('/Users/cailey/.nvm/versions/node/v10.13.0/lib/node_modules/character-gen');
+
 //local scripts
 const config = require("./config.json");
 const sillyPrompt = require('./commands/sillyprompt.js');
-const rhymes= require('./commands/rhymes.js');
+const rhymes = require('./commands/rhymes.js');
 const randomPoem = require('./commands/randomPoem.js');
 const definition = require('./commands/dictionary.js');
 const thesaurus = require('./commands/thesaurus.js');
 const urban = require('./commands/urban.js');
 const redditPrompt = require('./commands/redditPrompt.js');
+
 
 ///bot start
 client.on('ready', () => {
@@ -30,7 +34,7 @@ client.on("message", (message) => {
 
     //command to pull up a random poem from www.poemist.com API. 
     if (message.content.startsWith(prefix + "random poem")) {
-       randomPoem.get(message);
+        randomPoem.get(message);
     }
     //command to find words that rhyme with the search query. 
     //Results are grabbed from Datamuse API.
@@ -49,15 +53,28 @@ client.on("message", (message) => {
     if (message.content.startsWith(prefix + "synonym")) {
         thesaurus.get(message);
     }
-      //retrieve urban dictionary definition of search term
-      if (message.content.startsWith(prefix + "urban")) {
+    //retrieve urban dictionary definition of search term
+    if (message.content.startsWith(prefix + "urban")) {
         urban.get(message);
     }
-       //retrieve a random writing prompt from r/writingPrompts
-       if (message.content.startsWith(prefix + "reddit prompt")) {
+    //retrieve a random writing prompt from r/writingPrompts
+    if (message.content.startsWith(prefix + "reddit prompt")) {
         redditPrompt.get(message);
     }
+    //generate a random character archetype 
+    if (message.content.startsWith(prefix + "gen archetype")) {
+        message.channel.send(generate.basic());
+    }
+    //generate a random character appearance profile
+    if (message.content.startsWith(prefix + "gen appearance")) {
+        message.channel.send(generate.appearance());
+    }
+    //generate a random character attribute profile
+    if (message.content.startsWith(prefix + "gen character")) {
+        message.channel.send(generate.complex());
+    }
 
+ 
 });
 
 
